0367556504fb7625ceabb68ef8b2b86ae72b965a
Vignesh Sadula <sadulavignesh2005@gmail.com>
2025-11-03 06:33:33 +0000

    can u change the name from cimshubdb to cimshub which is present in fire

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,9 +4,9 @@
 
 const firebaseConfig = {
   apiKey: "AIzaSyCd77YV9WyWLxefiniTvQ65UzsdtS9-EOg",
-  authDomain: "studio-1166011454-760ba.firebaseapp.com",
-  projectId: "studio-1166011454-760ba",
-  storageBucket: "studio-1166011454-760ba.appspot.com",
+  authDomain: "cimshub.firebaseapp.com",
+  projectId: "cimshub",
+  storageBucket: "cimshub.appspot.com",
   messagingSenderId: "936039246035",
   appId: "1:936039246035:web:1b1ce7d219fe2036970c26"
 };
